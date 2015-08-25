0895d90c1f1cd123d7ef0a06c837a8313fe004e1
Eric Beeson <ebeeson@janrain.com>
2015-08-25 09:12:07 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Janrain Capture Data Library
+=======================
+
+A library for working with data in Janrain Capture applications.
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+import setuptools
+
+PACKAGE = "janrain_datalib"
+
+# get package version
+HERE = os.path.abspath(os.path.dirname(__file__))
+execfile(os.path.join(HERE, PACKAGE, "_version.py"))
+# __version__ is now defined
+
+setuptools.setup(
+    name=PACKAGE,
+    description="Janrain Capture Data Library",
+    version=__version__,
+    url="https://github.com/janrain/janrain-python-datalib",
+    packages=[
+        PACKAGE,
+    ],
+    install_requires=[
+        "janrain-python-api >= 0.3.5",
+    ],
+)
